902b9b441fa98f90ae004d6faadcaa6ede6d15a0
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-02-19 21:46:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,15 @@ $client = new Client('Z5R6TMf4M0FDuypDqcwQIwtt');
 // Get all Stories from the article folder
 $client->getStories(['starts_with' => 'blog']);
 $data = $client->getBody();
+echo $data["cv"] . PHP_EOL;
+print_r($data["rels"]);
+print_r($data["links"]);
 ?>
 <pre>
-    <?php print_r($data["rels"]); ?>
+    <?php 
+    echo $data["cv"] . PHP_EOL;
+    print_r($data["rels"]);
+    print_r($data["links"]);
+    ?>
 </pre>
 
