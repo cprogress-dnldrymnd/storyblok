649c74a8dd9c971e6df97656debe453afb483ce4
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-02-19 21:41:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
 // Require composer autoload
 require 'vendor/autoload.php';
-// Use the Storyblok\ManagementClient class
-use Storyblok\ManagementClient;
-// Use the ManagementClient class
-$managementClient = new ManagementClient('your-storyblok-oauth-token');
+// Use the Storyblok\Client class
+use Storyblok\Client;
+// Use the Client class
+$client = new Client('Z5R6TMf4M0FDuypDqcwQIwtt');
+
+// Get all Stories from the article folder
+$client->getStories(['starts_with' => 'article']);
+$data = $client->getBody();
+print_r($data["stories"]);
+echo $data["cv"] . PHP_EOL;
+print_r($data["rels"]);
+print_r($data["links"]);
