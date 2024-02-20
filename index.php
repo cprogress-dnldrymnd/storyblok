0b88f10a22c243a669f80ec08c57110c82bd4c93
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-02-21 00:28:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -56,7 +56,10 @@ function get_contents($contents)
                         $filename =  str_replace(".JPG", ".jpg", $filename);
                         $contents_var .= '<span class="blog-image"><img src="https://ten87.theprogressteam.co.uk/wp-content/uploads/2024/02/' . basename($filename) . '"/></span>';
                     } else if ($ar['type'] == 'list_item') {
-                        $contents_var .= '..';
+                        foreach ($ar['content'] as $content) {
+                            $contents_var .= '<li>ss';
+                            $contents_var .= '</li>';
+                        }
                     }
 
 
