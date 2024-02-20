894a801ec94b01d9f69df2a817a3db13e4bacd21
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-02-20 21:08:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -172,9 +172,9 @@ ___STORIES
 
 <?php
 require_once("../wp-load.php");
-require_once(ABSPATH . 'wp-admin/includes/media.php');
-require_once(ABSPATH . 'wp-admin/includes/file.php');
-require_once(ABSPATH . 'wp-admin/includes/image.php');
+require_once('../wp-admin/includes/media.php');
+require_once('../wp-admin/includes/file.php');
+require_once('../wp-admin/includes/image.php');
 
 foreach ($blog_array as $blog) {
     // Create post object
