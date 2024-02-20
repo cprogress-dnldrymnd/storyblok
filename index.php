870c3104767c7f1548714caf1666cf8f95a7a2f8
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-02-21 01:13:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -66,10 +66,12 @@ function loop_content($arr)
 
     foreach ($arr as $ar) {
 
-        if ($ar['marks'][0]['type'] == 'bold') {
-            $contents_var .= '<strong>';
-        } else if ($ar['marks'][0]['type']  == 'link') {
-            $contents_var .= '<a href="' . $ar['marks'][0]['attrs']['href'] . ' " target="' . $ar['marks'][0]['attrs']['target'] . ' ">';
+        if (isset($ar['marks'])) {
+            if ($ar['marks'][0]['type'] == 'bold') {
+                $contents_var .= '<strong>';
+            } else if ($ar['marks'][0]['type']  == 'link') {
+                $contents_var .= '<a href="' . $ar['marks'][0]['attrs']['href'] . ' " target="' . $ar['marks'][0]['attrs']['target'] . ' ">';
+            }
         }
 
 
@@ -90,11 +92,13 @@ function loop_content($arr)
             }
         }
 
+        if (isset($ar['marks'])) {
 
-        if ($ar['marks'][0]['type'] == 'bold') {
-            $contents_var .= '</strong>';
-        } else if ($ar['marks'][0]['type']  == 'link') {
-            $contents_var .= '</a>';
+            if ($ar['marks'][0]['type'] == 'bold') {
+                $contents_var .= '</strong>';
+            } else if ($ar['marks'][0]['type']  == 'link') {
+                $contents_var .= '</a>';
+            }
         }
     }
 
