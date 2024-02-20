ab15f642db186eea1ec215a2e8fd526a6e7a17a5
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-02-21 00:47:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -58,8 +58,9 @@ function get_contents($contents)
                         $filename =  str_replace(".JPG", ".jpg", $filename);
                         $contents_var .= '<span class="blog-image"><img src="https://ten87.theprogressteam.co.uk/wp-content/uploads/2024/02/' . basename($filename) . '"/></span>';
                     } else if ($ar['type'] == 'list_item') {
-                        foreach ($ar['content'] as $content2) {
+                        foreach ($ar['content'] as $key => $content2) {
                             $contents_var .= '<li>';
+                            $contents_var .= 'keysssss'.$key;
 
                             $contents_var .= get_contents($content2);
 
