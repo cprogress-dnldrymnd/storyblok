346f731d12ce09b5942019b6bd18d9166e1103cf
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-02-20 19:39:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -88,7 +88,6 @@ foreach ($stories as $story) {
 
     foreach ($blogPostType as $key => $blogPost) {
        echo $key;
-       echo $blogPost['_uid'];
        echo '<br>';
     }
     // $contents_var .= get_contents($outroText);
