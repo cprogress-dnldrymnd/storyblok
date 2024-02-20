8415f37718807ea618d09b2e1b5d7c16a9ac218f
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-02-20 21:37:29 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,10 @@ use Storyblok\Client;
 $client = new Client('Z5R6TMf4M0FDuypDqcwQIwtt');
 
 // Get all Stories from the article folder
+
+$page = $_GET['page'] ? $_GET['page'] : 1;
 $client->getStories([
-    'page' => 1,
+    'page' => $page,
     'per_page' => 20,
     'starts_with' => 'blog',
 ]);
